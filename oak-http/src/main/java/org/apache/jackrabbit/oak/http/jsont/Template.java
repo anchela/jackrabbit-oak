5ca3aa26811e0f06b52f98c432241a91d71a0f75
Marcel Reutegger <marcel.reutegger@gmail.com>
2021-03-25 18:15:11 +0100

    First few classes

---
+++ oak-http/src/main/java/org/apache/jackrabbit/oak/http/jsont/Template.java
@@ -0,0 +1,40 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jackrabbit.oak.http.jsont;
+
+import org.apache.jackrabbit.oak.api.PropertyState;
+import org.apache.jackrabbit.oak.api.Tree;
+import org.apache.jackrabbit.oak.api.Type;
+
+public class Template {
+
+    private final Tree templateTree;
+
+    public Template(Tree template) {
+        this.templateTree = template;
+    }
+
+    public boolean matches(Tree tree) {
+        PropertyState p = templateTree.getProperty("match");
+        if (p == null) {
+            return false;
+        }
+        String matchExpression = p.getValue(Type.STRING);
+        // TODO: implement
+        return false;
+    }
+}
